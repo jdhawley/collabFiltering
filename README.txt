f96038cc2b8f0207bf70812772cd16a2c526bdd1
Scott <scott@nine.is>
2017-12-12 18:55:12 -0600

    readme update

--- README.txt
+++ README.txt
@@ -5,3 +5,7 @@ prediction for part 1 or part 2.
 
 Once you have selected which part you want to use, you will be prompted to enter a row and a column to predict. Note,
 the rows and columns are zero-based.
+
+Part 1 uses a vector-based similarity function aggregated using a weighted sum as a memory-based collaborative filtering algorithm.
+
+Part 2 is a model based collaborative filtering algorithm that uses adjusted cosine similarity and a weighted sum to make its predictions.
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-input_filename = "testinput.txt"
+input_filename = "input.txt"
 test_row = 2
 
 
@@ -108,7 +108,7 @@ def calculate_top_half_recommendation(test_row, col_number, ratings, average_rat
     return total
 
 
-def get_predicted_score(row_number, col_number, ratings, average_ratings, weights):
+def get_predicted_score_memory(row_number, col_number, ratings, average_ratings, weights):
     """
     Predicts a rating for a row, column combination.
 
@@ -117,19 +117,136 @@ def get_predicted_score(row_number, col_number, ratings, average_ratings, weight
     top = calculate_top_half_recommendation(row_number, col_number, ratings, average_ratings, weights)
     return round(average_ratings[row_number] + (top / sum(list(map(abs, weights[row_number])))), 1)
 
+def calculate_model_numerator(query_user, query_index, ratings, items, weights):
+	"""
+	Calculates and returns the sum of (sim(i,N)*R(u,N))
+	"""
+	near, tmp = ([] for i in range(2))
+	for user in ratings:
+		near.append(user[query_index])
+	for idx in items:
+		far = []
+		for user in ratings:
+			far.append(user[idx])
+		tmp.append(get_sim(near,far,weights, query_user) * user[idx])
+	print("Numerator: ", tmp)
+	return sum(tmp)
+
+def calculate_model_denominator(query_user, query_index, ratings, items, weights):
+	"""
+	Calculates and returns the sum of (abs(sim(i,N))) where i is the item and N 
+	is the set of items 
+	"""
+	near, tmp = ([] for i in range(2))
+	for user in ratings:
+		near.append(user[query_index])
+	for idx in items:
+		far = []
+		for user in ratings:
+			far.append(user[idx])
+		tmp.append(abs(get_sim(near, far, weights, query_user)))
+	print("denominator: ", tmp)
+	return sum(tmp)
+
+def get_sim(near, far, averages, user_idx):
+	""" 
+	Near is the item that is being queried. Far is a similar item.
+	Returns the adjusted cosine similarity between near and far.
+	"""
+
+	print()
+	
+	num, runningNear, runningFar, denom = ([] for i in range(4))
+	for n in range(len(near)-1):
+		c = near[n] - averages[n]
+		d = far[n] - averages[n]
+		num.append(c*d)
+		print("c: ", c)
+		print("d: ", d)
+		runningNear.append(c ** 2)
+		runningFar.append(d ** 2)
+
+	numerator = sum(num)
+	denom = (sum(runningNear) ** .5) * (sum(runningFar) ** .5)
+	print("sim_num: ", numerator)
+	print("sim_den: ", denom)
+	print("num/den: ", numerator/denom)
+	if(denom == 0):
+		print("Divide by 0")
+		return 0
+	
+	return numerator / denom
+
+def get_predicted_score_model(user_num, item_num, ratings, weights):
+	"""
+	Predicts a rating for an item for a specific user using a model-
+	based approach. This is assignment 2.
+	
+	Implements a weighted sum.
+	"""
+	items = get_relevant_items(ratings[user_num], ratings)
+	top = calculate_model_numerator(user_num, item_num, ratings, items, weights)
+	bottom = calculate_model_denominator(user_num, item_num, ratings, items, weights)
+	if (bottom == 0):
+		print("Divide by 0!")
+		return 0
+	return top / bottom
+
+def get_relevant_items(user, ratings):
+	"""
+	Return each item (col) for which queryUser has a rating 
+	"""
+	ls = []
+	for idx in range(0,len(user)):
+		if(user[idx] != 0):
+			ls.append(idx)
+	return ls
+	
+'''
+def calculate_model_numerator(user):
+	Find list of all users who have ranked both items. Get all similarItems from that list.
+	Get the similarity value between queryItem and each similarItem. Multiply each similarity value
+	by the queryUser's ranking for similarItem. Sum the resulting list.
+	return sum()
+
+def calculate_model_denominator():
+	Find list of all users who have ranked both items. Get all similarItems from that list.
+	Get the similarity value between queryItem and each similarItem. Take the absolute value of each
+	similarity value, then return the sum of the list
+	sim_items = []
+	sim_vals = []
+	for sim_item in sim_items:
+		sim_vals.append(abs(sim(sim_item, query_item)))
+	return sum(sim_vals)
+'''
+
+def listSims():
+	return 
 
 def main():
-    print("Crunching numbers, please wait...\n")
-    ratings = get_user_ratings(input_filename)
-    average_ratings = get_averages(ratings)
-    weights = get_all_weights(test_row, ratings, average_ratings)
+	print("Crunching numbers, please wait...\n")
+	ratings = get_user_ratings(input_filename)
+	average_ratings = get_averages(ratings)
+	weights = get_all_weights(test_row, ratings, average_ratings)
 
-    while True:
-        row_number = int(input("Please enter the user (row) to predict: "))
-        col_number = int(input("Please enter the item (column) to predict: "))
-        predicted_score = get_predicted_score(row_number, col_number, ratings, average_ratings, weights)
-        print("The predicted score is: " + str(predicted_score) + "\n")
+	choice = int(input("Choose part 1 or 2: "))	
+	if(choice == 1):
+		while True:
+			row_number = int(input("Please enter the user (row) to predict: "))
+			col_number = int(input("Please enter the item (column) to predict: "))
+			predicted_score = get_predicted_score_memory(row_number, col_number, ratings, average_ratings, weights)
+			print("The predicted score is: " + str(predicted_score) + "\n")
+	else:
+		while True: 
+			row_number = int(input("Please enter the user (row) to predict: "))
+			col_number = int(input("Please enter the item (column) to predict: "))
+			predicted_score = get_predicted_score_model(row_number, col_number, ratings, average_ratings) + average_ratings[row_number]
+			print("The predicted score is: " + str(predicted_score) + "\n")
 
+def put_in_bucket(flt):
+	if (flt > 5):
+		return 5
+	return int(flt)	
 
 if __name__ == "__main__":
     main()
